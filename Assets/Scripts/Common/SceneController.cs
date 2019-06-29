d3df26d925eeb4e63e166fe04807836d3e8a1f90
sorararan <masolainota@gmail.com>
2019-06-29 18:17:37 +0900

    シーン整理、不要なボールを消す

---
+++ Assets/Scripts/Common/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            if (SceneManager.GetActiveScene().name == "Title"){
+                SceneManager.LoadScene ("Scenes/Main");
+            }
+		}
+
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if (SceneManager.GetActiveScene().name == "Main"){
+                SceneManager.LoadScene ("Scenes/Title");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Main/BallController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    void Update() {
+        if(this.transform.position.x <= -9.5f){
+            Destroy(this.gameObject);
+        }
+    }
+}
